663e94a8b2c81b03b260f1f8cfd822d8b9fe0a02
yawneazy <60025410+yawneazy@users.noreply.github.com>
2025-08-19 22:35:02 -0700

    added logo, nav and form basic elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import valenciaAcctLogo from './images/valencia-acct-logo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <header>
+      <div class="header">
+        <div class="logo-container">
+        <img class="main-logo" src={valenciaAcctLogo} alt="valencia-acct-logo"/>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#meettheteam">Meet the Team</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#testimonials">Testimonials</a></li>
+            <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+      </div>
+      <div class="phone-number-container">
+        <p>(555)555-5555</p>
+      </div>
+    </header>
+    <main>
+      <div class="services-container">
+
+      </div>
+    </main>
+    <footer>
+      <form id="consultation-form">
+        <h2>Get in touch!</h2>
+        <label for = "first-name">First Name<span class="required">*</span></label>
+        {/* required */}
+        <input id="first-name" name="first-name" type="text" />
+
+        <label for = "first-name">Last Name<span class="required">*</span></label>
+        {/* required */}
+        <input id="first-name" name="first-name" type="text" />
+
+        <label for = "email">Email<span class="required">*</span></label>
+        {/* required */}
+        <input id="email" name="email" type="text" />
+
+        <label for = "message">Message<span class="required">*</span></label>
+        {/* required */}
+        <input id="message" name="message" type="text" />
+
+        <div class="submit-btn-container">
+          <button type="submit" id="submit">Submit</button>
+        </div>
+      </form>
+    </footer>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +74,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
