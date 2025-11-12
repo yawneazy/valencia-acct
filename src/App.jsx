ec9229072d235a7d47848523afd5812bd24cf81d
yawneazy <60025410+yawneazy@users.noreply.github.com>
2025-11-11 21:51:38 -0700

    incorporated new colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import valenciaAcctLogo from './images/valencia-acct-logo.png'
+import valenciaAcctLogo from './images/main-logo-no-bckrd.jpg'
 
 function App() {
 
@@ -8,12 +8,13 @@ function App() {
     <>
     <header>
       <div className="header">
+      <div className="logo-nav-container">
         <div className="logo-container">
         <img className="main-logo" src={valenciaAcctLogo} alt="valencia-acct-logo"/>
         </div>
         <nav>
           <ul>
-            <li><a href="#meettheteam">Meet the Team</a></li>
+            <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
             <li><a href="#testimonials">Testimonials</a></li>
             <li><a href="#contact">Contact</a></li>
@@ -23,10 +24,49 @@ function App() {
       <div className="request-consultation-btn">
         <button>Request a Consultation</button>
       </div>
+      </div>
     </header>
     <main>
-      <section className="main-services-info">
-        <p>At Valencia Accounting & Bookkeeping, our mission is to empower 
+      <section className="main-bio">
+        <p>Gabrielle Valencia Brosnan is an accomplished CTEC-licensed professional 
+          with a Bachelor's degree in Accounting. She brings 10 years of intensive, 
+          hands-on experience in tax preparation and accounting to her clients, providing
+          expert and efficient financial solutions.</p>
+      </section>
+<h1>Our Services</h1>
+      <section className="cards">
+        <article className="card">
+          <h3>Tax Preparation & Planning</h3>
+          <p>We ensure accurate and timely filing while proactively seeking strategies
+             to minimize your tax liability</p>
+        </article>
+
+        <article className="card"> 
+          <h3>Business Accounting & Bookkeeping</h3>
+          <p>Our core accounting services provide a clear, real-time picture of your financial performance, allowing you to make better business decisions</p>
+        </article>
+        
+        <article className="card">
+          <h3>Business Advisory & Consulting</h3>
+          <p>Our core accounting services provide a clear, real-time picture of your financial performance, allowing you to make better business decisions</p>
+        </article>
+
+        <article className="card">
+          <h3>Personal Financial Services</h3>
+          <p>We offer dedicated consulting to help individuals gain control of their money, reduce debt, and build a strong foundation for their financial future</p>
+        </article>
+
+        <article className="card">
+          <h3>Professional Notary Services</h3>
+          <p>As certified Notary Publics, we offer prompt and reliable service for verifying identity and witnessing signatures on legal documents</p>
+        </article>
+
+      </section>
+    </main>
+    <footer>
+    <img className="main-logo" src={valenciaAcctLogo} alt="valencia-acct-logo"/>
+      <div className='footer-container'>
+               <p>At Valencia Accounting & Bookkeeping, our mission is to empower 
           individuals and businesses to take control of their financial 
           futures. We leverage our expertise in accounting to help individuals
           create effective budgets, manage and reduce debt, and provide clarity 
@@ -35,55 +75,27 @@ function App() {
           growth. Additionally, we provide expert tax preparation services for 
           both personal and corporate entities, helping our clients navigate the 
           complexities of taxes while ensuring long-term financial stability and 
-          success.</p>
-      </section>
-
-      <section className="cards">
-        <article className="card">
-          <h3>Bookkeeping/Accounting</h3>
-          <p>We offer a range of packages that can pair well on all aspects of your 
-            company's accounting needs along with any additional needs. </p>
-        </article>
-
-        <article className="card"> 
-          <h3>Notary</h3>
-          <p>We assure the parties of a transaction that a document is authentic, 
-            and can be trusted. This includes of vetting, certifying and 
-            record-keeping.</p>
-        </article>
-        
-        <article className="card">
-          <h3>Personal Finance</h3>
-          <p>We assist our clients with their own finances such as helping with 
-            budgeting, learning how to get out of debt, and balancing accounts. </p>
-        </article>
-
-        <button className="see-all-services-btn"> See All Services</button>
-      </section>
-    </main>
-    <footer>
-      <form id="consultation-form">
+          success.</p> 
+      </div>
+     {/*  <form id="consultation-form">
         <h2>Get in touch!</h2>
         <label for = "first-name">First Name<span class="required">*</span></label>
-        {/* required */}
         <input id="first-name" name="first-name" type="text" />
 
         <label for = "first-name">Last Name<span class="required">*</span></label>
-        {/* required */}
         <input id="first-name" name="first-name" type="text" />
 
         <label for = "email">Email<span class="required">*</span></label>
-        {/* required */}
         <input id="email" name="email" type="text" />
 
         <label for = "message">Message<span class="required">*</span></label>
-        {/* required */}
         <input id="message" name="message" type="text" />
 
         <div className="submit-btn-container">
           <button type="submit" id="submit">Submit</button>
         </div>
       </form>
+    */}
     </footer>
     </>
   )
